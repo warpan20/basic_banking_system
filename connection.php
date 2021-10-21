c8ba0adb9503271d31ad88f6f0582379fafda76c
Arpan Sen <warpan2020@gmail.com>
2021-10-21 16:53:05 +0530

    first commit

---
+++ connection.php
@@ -0,0 +1,28 @@
+<?php
+
+$username = "id17785097_sparkfoundation";
+$password="federer#123SWISS";
+$server ="localhost";
+$db = "id17785097_warpan";
+
+
+$con = mysqli_connect($server,$username,$password,$db);
+
+/*if($con)
+//echo "Connection successful";
+{
+    ?>
+    <script>
+        alert('Connection Successful');
+    </script>
+    <?php
+}
+else
+{
+    echo "No connection";
+    die("no connection".mysqli_connect_error());//exit
+}
+*/
+
+
+?>
